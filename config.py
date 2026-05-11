3ed46b5892136b9ef42992b9e393af439e66c265
Support Vimba boss <moto34091@gmail.com>
2026-05-11 03:10:38 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,10 @@
 import os
 
-# 🔐 TOKEN BOT (mettre dans ENV Railway recommandé)
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-
-# 💰 LIEN PARTENAIRE
-POCKET_LINK = "https://pocket-friends.co/r/ffpccthlup"
-
-# 🎁 CODE PROMO
-PROMO_CODE = "FRIENDFFPCCTHLUP"
-
-# 👑 ADMIN ID
 ADMIN_ID = 5161872804
 
-# ⚙️ LIMITES
+POCKET_LINK = "https://pocket-friends.co/r/ffpccthlup"
+PROMO_CODE = "FRIENDFFPCCTHLUP"
+
 FREE_SIGNALS_LIMIT = 2
 COOLDOWN = 10
