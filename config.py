435f1f418b3bad6f4db33c0fade6d511f670848a
Support Vimba boss <moto34091@gmail.com>
2026-05-11 03:02:59 +0200

    Refactor BOT_TOKEN to use environment variable
    Updated BOT_TOKEN to use environment variable for security.

--- config.py
+++ config.py
@@ -1,9 +1,17 @@
-BOT_TOKEN = "8652341719:AAGIIkyqbVHNPYVUghPUn4MA_kIcrkyJfpM"
+import os
 
+# 🔐 TOKEN BOT (mettre dans ENV Railway recommandé)
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+
+# 💰 LIEN PARTENAIRE
 POCKET_LINK = "https://pocket-friends.co/r/ffpccthlup"
+
+# 🎁 CODE PROMO
 PROMO_CODE = "FRIENDFFPCCTHLUP"
 
+# 👑 ADMIN ID
 ADMIN_ID = 5161872804
 
+# ⚙️ LIMITES
 FREE_SIGNALS_LIMIT = 2
 COOLDOWN = 10
