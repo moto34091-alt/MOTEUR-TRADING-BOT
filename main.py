2622c577b0b27d1830e65a9563ae3924957d751f
Support Vimba boss <moto34091@gmail.com>
2026-05-11 03:20:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes
 
 # 🔐 TOKEN (ENV)
-BOT_TOKEN = os.getenv("8652341719:AAGIIkyqbVHNPYVUghPUn4MA_kIcrkyJfpM")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 logging.basicConfig(level=logging.INFO)
 
