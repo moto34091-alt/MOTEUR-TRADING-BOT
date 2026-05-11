b677d8e09bd1406df35b6f5ee8dbea142c9f68fe
Support Vimba boss <moto34091@gmail.com>
2026-05-11 02:54:30 +0200

    Update BOT_TOKEN to a hardcoded value
    Replaced BOT_TOKEN with a hardcoded value.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from telegram.ext import (
 )
 
 # 🔐 TOKEN
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("8652341719:AAGIIkyqbVHNPYVUghPUn4MA_kIcrkyJfpM")
 
 logging.basicConfig(level=logging.INFO)
 
